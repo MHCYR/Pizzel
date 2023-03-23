715a3a3574f7a1d9e35ab4e71908900db19563a8
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2023-03-23 13:16:04 -0600

    feat(App): add register form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import RegisterForm from './components/RegisterForm';
 
+function App() {
   return (
-    <div className="w-screen h-screen bg-stone-800">
+    <div className="w-screen h-screen bg-stone-800 flex justify-center items-center">
+      <RegisterForm />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
