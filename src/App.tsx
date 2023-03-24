2fe232152dc018560a63dba21d91d36793c4b4c5
Mohacyr Rojas <mohacyr.rojas.contractor@bbva.com>
2023-03-23 20:17:42 -0600

    feat(App): create routes and grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import RegisterForm from './components/RegisterForm';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-stone-800 flex justify-center items-center">
-      <RegisterForm />
+    <div className="w-screen h-screen bg-stone-800 grid grid-cols-3 grid-rows-6">
+      <BrowserRouter>
+        <Navbar className="col-span-3 h-10 bg-gray-600" />
+        <Routes>
+          <Route path="/" element={<RegisterForm className="col-start-2 row-span-4 place-self-center" />} />
+          <Route path="/login" element={<div>aqui va el componente de quike</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
